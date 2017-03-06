9f745346bfe660f5539614ac963949e4f282dc21
Tibenji <tim.dreger@gmx.de>
2017-03-06 13:40:47 +0100

    Update

--- Assets/Scripts/Paragraph.js
+++ Assets/Scripts/Paragraph.js
@@ -1,12 +1,13 @@
 ﻿public class Paragraph {
   var txt: String;
-  var visited: boolean;
-  var optionCount: int;
-  var optionArray: Array;
-
+  var visited:boolean;
+  var optionCount:int;
+  var optionArray:Array;
+  var battleEncounter:boolean;
   // Für weitere Abschnitte
   var extraPGCheck: boolean = false;
   var extraPG: Array;
+
 
   // Erstellt einen neuen Paragraphen
   function Paragraph(txt: String) {
@@ -15,19 +16,33 @@
     optionArray = [];
     extraPG = [];
     // kann vielleicht weg
-    seen = false;
+    battleEncounter = false;
   }
+
 
   // Fügt eine Antwortmöglichkeit hinzu
   function addOption(optionTxt:String, requirements:String, nextPG:Paragraph) {
     var option: Hashtable = new Hashtable();
-
+    option["hasRandomFactor"] = false;
     option["optionTxt"] = optionTxt;
     option["requirements"] = requirements;
-    // Mensch, eigentlich will ich nur ne Reference, aber wenn nextPG ein String ist, geht gar nichts. :(
     option["nextPG"] = nextPG;
     optionCount++;
     optionArray.push(option);
+  }
+
+  // Für prozentuelle Antworten
+      function addOption(optionTxt:String, requirements:String, nextPG1:Paragraph, nextPG2:Paragraph, percent:int) {
+      var option: Hashtable = new Hashtable();
+      option["hasRandomFactor"] = true;
+      option["optionTxt"] = optionTxt;
+      option["requirements"] = requirements;
+      option["nextPG1"] = nextPG1;
+      option["percent1"] = 100-percent;
+      option["nextPG2"] = nextPG2;
+      option["percent2"] = percent;
+      optionCount++;
+      optionArray.push(option);
   }
 
   // fügt einen weiteren Textteil hinzu
@@ -36,46 +51,9 @@
         extraPGCheck = true;
         extraPG.push(extraPGTxt);
   }
+
+  function addProcent(){
+
   }
 
-     /*
-public class Paragraph {
-  var txt: String;
-  var visited: boolean;
-  var optionCount: int;
-  var optionArray: Array;
-
-  // Für weitere Abschnitte
-  var extraPGCheck: boolean = false;
-  var extraPG: Array;
-
-  // Erstellt einen neuen Paragraphen
-  function Paragraph(txt: String) {
-    this.txt = txt;
-    optionCount = 0;
-    optionArray = [];
-    extraPG = [];
-    // kann vielleicht weg
-    seen = false;
-  }
-
-  // Fügt eine Antwortmöglichkeit hinzu
-  function addOption(optionTxt:String, requirements:String, nextPG:String) {
-    var option: Hashtable = new Hashtable();
-
-    option["optionTxt"] = optionTxt;
-    option["requirements"] = requirements;
-    option["nextPG"] = nextPG;
-    optionCount++;
-    optionArray.push(option);
-  }
-
-  // fügt einen weiteren Textteil hinzu
-  // das ist bei langen Texten sinnvoll!
-  function addExtraPG(extraPGTxt: String) {
-        extraPGCheck = true;
-        extraPG.push(extraPGTxt);
-  }
-}    
-
-    */
+}
